2d2b83d0cde56d297a736a9265284d0f7babbd2f
AtesComp <atescomp@gmail.com>
2025-07-28 02:51:32 -0400

    Add panel * Align collection table elements * Update color management * Standardize variable names * Manage die color for missing colored die

--- js/random.js
+++ js/random.js
@@ -1,91 +1,81 @@
-var RandomArray = new Uint32Array(1);
-var CollectionRow = 0;
-var Timestamp = 0;
-var SortAscend = [ false, false, false ];
+var g_arrRandom = new Uint32Array(1);
+var g_iCollectionRow = 0;
+var g_strTimestamp = 0;
+var g_arrSortAscend = [ false, false, false ];
+const g_carrDieFaces = [6, 8, 12, 20];
+const g_carrDieColors = ["Blue","Green","Purple","Red","White","Yellow", "Sequence", "OffWhite"];
 
-function getRandom(min, max) {
-  window.crypto.getRandomValues(RandomArray);
-  var RandomNumber = RandomArray[0] % (max-min+1) + min;
-  return RandomNumber;
-}
-
-function printRoll() {
-  var pStr = document.getElementById('dieCntSave');
-  //pStr.innerHTML = 'You rolled ' + document.getElementById('dieCntSave').value + ' dice.';
-  console.log('You rolled ' + document.getElementById('dieCntSave').value + ' dice.')
+function getRandom(iMin, iMax) {
+  window.crypto.getRandomValues(g_arrRandom);
+  let iRandomNumber = g_arrRandom[0] % (iMax - iMin + 1) + iMin;
+  return iRandomNumber;
 }
 
 function getDiceRoll() {
-  var myCnt = parseInt( document.getElementById('dieCnt').value.trim() );
+  var myCount = parseInt( document.getElementById('dieCount').value.trim() );
   var mySides = parseInt( document.getElementById('dieSide').value.trim() );
-  var myRollResult = document.getElementById('dieRollResult');
-  const dieFaces = [6,8,12,20];
-  var colors = ["Blue","Green","Purple","Red","White","Yellow", "Sequence", "OffWhite"];
-  if (myCnt < 1 || mySides < 1) {
-    myRollResult.innerHTML = '<p>No dice! Check count and sides!</p>';
-//    document.getElementById('dieCnt').value = '';
-//    document.getElementById('dieCntSave').value = 0;
-//    document.getElementById('dieSide').value = '';
-//    printRoll();
+  var myRollImage = document.getElementById('dieRollImage');
+  myRollImage.innerHTML = '';
+  if (myCount < 1 || mySides < 1) {
+    myRollImage.innerHTML = '<h2><b>No dice! Check count and sides are greater than 0!</b></h2>';
     return;
   }
 
-  var myResult = '<h1>'; // ...start result text
-  var myVal = 0;
-  var myBaseColor = document.getElementById('dieColor').value.trim();
-  for (i=0; i<myCnt; i++) {
-    var myColor = myBaseColor;
-    var myDie = getRandom(1, mySides);
-    if (mySides == 6) {
-      if (myColor == 6) {
-        myColor = i % 6;
-      }
-    }
+  let myResultHTML = '<h1>'; // ...start result text
+  let myResultVal = 0;
+  let myBaseColor = document.getElementById('dieColor').value.trim();
+  let myColor = myBaseColor;
+  // Only White exists for non 6 sided-die types...
+  if (mySides !== 6) myColor = g_carrDieColors.indexOf("White");
 
-    var mySided = pad(mySides, 2);
-    if ( dieFaces.indexOf(mySides) > -1 ) {
-      myResult += '<img src="images/D' + mySided + '_' + colors[myColor] + '_' + myDie + '.png" alt="' + myDie + '" />';
+  for (i = 0; i < myCount; i++) {
+    // On 6 sided-die with sequence coloring...
+    if (mySides === 6 && myBaseColor == 6) myColor = i % 6; // ...cycle the first 6 colors
+
+    let myDieRoll = getRandom(1, mySides);
+    if ( g_carrDieFaces.indexOf(mySides) > -1 ) { // ...use a well-known die image...
+      let mySided = pad(mySides, 2);
+      myResultHTML += '<img src="images/D' + mySided + '_' + g_carrDieColors[myColor] + '_' + myDieRoll + '.png" alt="' + myDieRoll + '" />';
     }
-    else if (mySides < 20) {
-      myResult += '<img src="images/DTRI_Paper_' + myDie + '.png" alt="' + myDie + '" />';
+    else if (mySides < 20) { // ...use a basic triangular die image...
+      myResultHTML += '<img src="images/DTRI_Paper_' + myDieRoll + '.png" alt="' + myDieRoll + '" />';
     }
-    else {
-      myResult += '' + myDie + ' ';
+    else { // ...no die image--just display the value...
+      myResultHTML += '' + myDieRoll + ' ';
     }
-    myVal += myDie;
+    myResultVal += myDieRoll;
   } // ...end loop
-  myResult += '</h1>'; // ...end result text
-  myRollResult.innerHTML = myResult;
-  myTotal = '<b>Total: ' + myVal + '</b>';
+
+  myResultHTML += '</h1>'; // ...end result text
+  myRollImage.innerHTML = myResultHTML;
+  myTotal = '<b>' + myResultVal + '</b>';
   document.getElementById('dieTotal').innerHTML = myTotal;
-  document.getElementById('dieCnt').value = myCnt;
-  document.getElementById('dieCntSave').value = myCnt;
+  document.getElementById('dieCount').value = myCount;
   document.getElementById('dieSide').value = mySides;
 
   var bCollect = document.getElementById('collectRoll').value.trim();
   if (bCollect == 'true') {
     document.getElementById('reportRollsBody').innerHTML +=
-      '<tr id="roll' + CollectionRow + '">' +
-        '<td><input type="checkbox" name="cbox' + CollectionRow + '"/></td>' +
-        '<td>' + myVal + '</td>' +
-        '<td>' + Timestamp + '</td>' +
+      '<tr id="roll' + g_iCollectionRow + '">' +
+        '<td><input type="checkbox" name="cbox' + g_iCollectionRow + '"/></td>' +
+        '<td>' + myResultVal + '</td>' +
+        '<td>' + g_strTimestamp + '</td>' +
         '<td>' +
-          '<button onclick="removeRow(\'roll' + CollectionRow + '\')" ' +
+          '<button onclick="removeRow(\'roll' + g_iCollectionRow + '\')" ' +
                   'type="button" ' +
           '>&#10060;</button>' +
         '</td>' +
       '</tr>';
-    CollectionRow += 1;
+    g_iCollectionRow += 1;
   }
-//  printRoll();
 }
 
 function getTimestamp() {
-  Timestamp = (new Date()).toISOString();
+  g_strTimestamp = (new Date()).toISOString();
 }
 
 function setTimestamp(valTime) {
-  document.getElementById('timestamp').innerHTML = Timestamp;
+  document.getElementById('timestamp').innerHTML = g_strTimestamp;
 }
 
 function removeRow(strRow) {
@@ -98,18 +88,18 @@ function setCollect() {
   var bCollect = document.getElementById('collectRoll').value.trim();
   if (bCollect == 'true') { // ...changing to false...
       document.getElementById('collectRoll').value = 'false';
-      CollectionRow = 0;
+      g_iCollectionRow = 0;
       document.getElementById('reportRolls').innerHTML = '';
   }
   else { // ...changing to true...
       document.getElementById('collectRoll').value = 'true';
-      CollectionRow = 0;
+      g_iCollectionRow = 0;
       document.getElementById('reportRolls').innerHTML =
         '<thead>' +
           '<th><button type="button" onclick="sortTable(0)">Mark</button></th>' +
           '<th><button type="button" onclick="sortTable(1)">Roll</button></th>' +
           '<th><button type="button" onclick="sortTable(2)">Time</button></th>' +
-          '<th>Remove</th>' +
+          '<th></th>' +
         '</thead>' +
         '<tbody id="reportRollsBody"></tbody>';
   }
@@ -120,7 +110,7 @@ function sortTable(iCol) {
     let tableBody = document.getElementById('reportRollsBody');
     // Get the rows, removing the table header....
     let rows = Array.from(table.rows).slice(1);
-    SortAscend[iCol] = ! SortAscend[iCol]; // ...flip Ascending / Descending
+    g_arrSortAscend[iCol] = ! g_arrSortAscend[iCol]; // ...flip Ascending / Descending
 
     // Sort the rows...
     let rowsSorted = rows.sort(
@@ -153,7 +143,7 @@ function sortTable(iCol) {
         // Otherwise, try sort by string values...
         else iComp = cellA.innerText.localeCompare(cellB.innerText);
 
-        return SortAscend[iCol] ? iComp : -iComp;
+        return g_arrSortAscend[iCol] ? iComp : -iComp;
       }
     );
 
